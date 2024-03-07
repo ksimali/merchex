1bbaaa03d63c5c8378c5548058a1b6d12274dd4b
ksimali <cassim.ali@outlook.fr>
2024-03-06 20:37:05 -0500

    Set up custom user

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    profile_photo = models.ImageField(verbose_name='Photo de profil')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ('username',)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
